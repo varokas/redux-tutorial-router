a21861877d0118dced1c88dbb99fc604dd89dbe7
Varokas Panusuwan <varokas@panusuwan.com>
2017-04-09 23:02:11 +0700

    add submit

---
+++ src/components/posts_new.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+
+class PostsNew extends Component {
+  render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>New posts</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
