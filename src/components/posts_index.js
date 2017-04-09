33fea817aa85677ae310a4057c9b9e8af5c2d313
Varokas Panusuwan <varokas@panusuwan.com>
2017-04-09 23:33:47 +0700

    add posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -10,6 +10,17 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      )
+    });
+  }
+
   render() {
     return (
       <div>
@@ -18,16 +29,17 @@ class PostsIndex extends Component {
             Add a Post
           </Link>
         </div>
-        List of blogs
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchPosts }, dispatch);
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
 }
 
-const actions = { fetchPosts }
-
-export default connect(null, actions)(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
