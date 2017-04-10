34abdbca7419deb9c9f0043dbc48744204bfe831
Varokas Panusuwan <varokas@panusuwan.com>
2017-04-10 09:07:11 +0700

    add post show

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ import App from './components/app';
 
 import PostsIndex from './components/posts_index'
 import PostsNew from './components/posts_new'
+import PostsShow from './components/posts_show'
 
 export default (
   <Route path="/"  component={App}>
     <IndexRoute component={PostsIndex}/>
     <Route path="posts/new" component={PostsNew}/>
+    <Route path="posts/:id" component={PostsShow}/>
   </Route>
 )
